34603332ce8303e024dbcf6ceb59e2d6f623e649
Victor Nobrega <157187503+victornobrega6@users.noreply.github.com>
2025-04-16 20:25:07 -0300

    Update test_math_utils.py

--- test_math_utils.py
+++ test_math_utils.py
@@ -1,5 +1,5 @@
 import unittest
-from math_utils import add, subtract, multiply_matrix
+from math_utils import add, subtract, multiply
 
 class TestMathUtils(unittest.TestCase):
     def test_add(self):
